706a15f887f10a023b78aebebebe93ba036168b5
Alan Silva <80364531+Alan-ocs@users.noreply.github.com>
2022-10-23 01:16:35 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "barbero-devops-iac"
+  project = "aocs-devops-iac"
   region  = "us-central1"
   zone    = "us-central1-c"
   credentials = "${file("serviceaccount.yaml")}"
@@ -10,25 +10,25 @@ resource "google_folder" "Financeiro" {
   parent       = "organizations/540829645030"
 }
 
-resource "google_folder" "SalesForce" {
-  display_name = "SalesForce"
+resource "google_folder" "Recursos Humanos" {
+  display_name = "Recursos Humanos"
   parent       = google_folder.Financeiro.name
 }
 
 resource "google_folder" "Desenvolvimento" {
   display_name = "Desenvolvimento"
-  parent       = google_folder.SalesForce.name
+  parent       = google_folder.Recursos Humanos.name
 }
 
 resource "google_folder" "Producao" {
   display_name = "Producao"
-  parent       = google_folder.SalesForce.name
+  parent       = google_folder.Recursos Humanos.name
 }
 
 
-resource "google_project" "barberosa2-salesforce-dev" {
-  name       = "SalesForce-Dev"
-  project_id = "barberosa2-salesforce-dev"
+resource "google_project" "aocs-Recursos Humanos-dev" {
+  name       = "Recursos Humanos-Dev"
+  project_id = "aocs-Recursos Humanos-dev"
   folder_id  = google_folder.Desenvolvimento.name
   auto_create_network=false
   billing_account = "018973-A8340F-83D8E5"
